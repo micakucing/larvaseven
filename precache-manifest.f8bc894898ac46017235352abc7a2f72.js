c79758491d6d46effe85ef76af996b621240ca48
mica neko <micakucing@gmail.com>
2019-03-13 15:03:40 +0700

    Updates

---
+++ precache-manifest.f8bc894898ac46017235352abc7a2f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba1b8db2d4ca541dca70",
+    "url": "/larvaseven/static/js/runtime~main.58d43a49.js"
+  },
+  {
+    "revision": "2c7ecb96717276434761",
+    "url": "/larvaseven/static/js/main.1aade733.chunk.js"
+  },
+  {
+    "revision": "e3e521d73e415f802a7e",
+    "url": "/larvaseven/static/js/2.b77d4059.chunk.js"
+  },
+  {
+    "revision": "2c7ecb96717276434761",
+    "url": "/larvaseven/static/css/main.9519a473.chunk.css"
+  },
+  {
+    "revision": "e3e521d73e415f802a7e",
+    "url": "/larvaseven/static/css/2.4f8c03a3.chunk.css"
+  },
+  {
+    "revision": "90d0129bad0634250ed8efd6245d50c6",
+    "url": "/larvaseven/index.html"
+  }
+];
