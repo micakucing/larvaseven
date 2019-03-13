e7944649f62c448651988c018861b1f530c423c9
mica neko <micakucing@gmail.com>
2019-03-13 20:23:15 +0700

    l

--- src/route/midletree.js
+++ src/route/midletree.js
@@ -10,47 +10,47 @@ class midltre extends Component {
 
         return (
           <div>
-        <section class="site-section">
-      <div class="container">
-        <div class="row mb-5 justify-content-center">
-          <div class="col-md-8 text-center">
-            <h2 class="text-black h1 site-section-heading text-center">Featured Projects</h2>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, itaque neque, delectus odio iure explicabo.</p>
+        <section className="site-section">
+      <div className="container">
+        <div className="row mb-5 justify-content-center">
+          <div className="col-md-8 text-center">
+            <h2 className="text-black h1 site-section-heading text-center">Featured Projects</h2>
+            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, itaque neque, delectus odio iure explicabo.</p>
           </div>
         </div>
       </div>
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-6 col-lg-4">
-            <a href="/product" class="media-1">
-              <img src="https://colorlib.com/preview/theme/chimper/images/about_1.jpg" alt="Image" class="img-fluid">
-              <div class="media-1-content">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 col-lg-4">
+            <a href="/product" className="media-1">
+              <img src="https://colorlib.com/preview/theme/chimper/images/about_1.jpg" alt="Image" className="img-fluid" />
+              <div className="media-1-content">
                 <h2>Project Name 1</h2>
-                <span class="category">Web Application</span>
+                <span className="category">Web Application</span>
               </div>
             </a>
           </div>
-          <div class="col-md-6 col-lg-4">
-            <a href="/product" class="media-1">
-              <img src="https://colorlib.com/preview/theme/chimper/images/about_1.jpg" alt="Image" class="img-fluid">
-              <div class="media-1-content">
+          <div className="col-md-6 col-lg-4">
+            <a href="/product" className="media-1">
+              <img src="https://colorlib.com/preview/theme/chimper/images/about_1.jpg" alt="Image" className="img-fluid"/>
+              <div className="media-1-content">
                 <h2>Project Name 2</h2>
-                <span class="category">Branding</span>
+                <span className="category">Branding</span>
               </div>
             </a>
           </div>
-          <div class="col-md-6 col-lg-4">
-            <a href="/product" class="media-1">
-              <img src="https://colorlib.com/preview/theme/chimper/images/about_1.jpg" alt="Image" class="img-fluid">
-              <div class="media-1-content">
+          <div className="col-md-6 col-lg-4">
+            <a href="/product" className="media-1">
+              <img src="https://colorlib.com/preview/theme/chimper/images/about_1.jpg" alt="Image" className="img-fluid"/>
+              <div className="media-1-content">
                 <h2>Project Name 3</h2>
-                <span class="category">Website</span>
+                <span className="category">Website</span>
               </div>
             </a>
           </div>
 
-          <div class="col-12 text-center mt-5">
-            <a href="/portfolio" class="btn btn-primary btn-md">Show All Works</a>
+          <div className="col-12 text-center mt-5">
+            <a href="/portfolio" className="btn btn-primary btn-md">Show All Works</a>
           </div>
 
           
