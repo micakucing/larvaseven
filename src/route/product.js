91608a4c5b58d76d72e2a85707e64f01598668d3
mica neko <micakucing@gmail.com>
2019-03-14 23:33:21 +0700

    l

--- src/route/product.js
+++ src/route/product.js
@@ -6,13 +6,58 @@ import $ from "jquery";
 import Testi from "./testi";
 import Foot from "./foot";
 import Nav from "./nav";
+import 'react-photoswipe/lib/photoswipe.css';
+import {PhotoSwipeGallery} from 'react-photoswipe';
 
 class product extends Component {
+constructor(props) {
+        super(props);
+    this.timeout = null;
 
+    }
     componentDidMount() {
     }
+
+
+
     render() {
-    
+   
+
+ const items = [
+  {
+    src: 'https://colorlib.com/preview/theme/chimper/images/img_1.jpg',
+    thumbnail: 'https://colorlib.com/preview/theme/chimper/images/img_1.jpg',
+    w: 1200,
+    h: 900,
+    title: 'Image 1'
+  },
+  {
+    src: 'https://colorlib.com/preview/theme/chimper/images/img_2.jpg',
+    thumbnail: 'https://colorlib.com/preview/theme/chimper/images/img_2.jpg',
+    w: 1200,
+    h: 900,
+    title: 'Image 2'
+  },
+  {
+    src: 'https://colorlib.com/preview/theme/chimper/images/img_3.jpg',
+    thumbnail: 'https://colorlib.com/preview/theme/chimper/images/img_3.jpg',
+    w: 1200,
+    h: 900,
+    title: 'Image 3'
+  }
+];
+
+const options = {
+  //http://photoswipe.com/documentation/options.html
+};
+
+const getThumbnailContent = (item) => {
+  return (
+    <div>
+    <img src={item.thumbnail}   className="img-fluid mb-4"/>
+  </div>
+  );
+}
         return (
 
 
@@ -22,6 +67,7 @@ class product extends Component {
    .site-blocks-cover.inner-page-cover, .site-blocks-cover.inner-page-cover>.container>.row {
       min-height: 280px;
     }
+
     .text-center h1{
       position: absolute;
     top: 12px;
@@ -46,8 +92,40 @@ class product extends Component {
     margin-top: 9px;
     letter-spacing: .5px;
     }
-
+    .site-navbar{
+      z-index: 0;
+    }
+     .pswp__button--share{
+  display: none;
+ }
+#lpsa{
+  margin-bottom: 3px;
+  text-align: right;
+}
+.mb-4{
+  cursor: zoom-in;
+}
    .site-blocks-cover.overlay:before{background-color: #FD1C15}
+   p { 
+    margin-bottom: 2rem;
+}
+   @media (min-width: 768px){
+ 
+.col-md-7 {
+    -webkit-flex: 0 0 58.333333%;
+    flex: 0 0 100%;
+    max-width: 100%;
+}
+.col-md-7 img{
+
+  width: 100%;
+}
+.col-md-4 {
+    -webkit-flex: 0 0 33.333333%;
+    flex: 0 0 50%;
+    max-width: 50%;
+}
+}
    @media (max-width: 992px) {
  .text-center h1{
     font-size: 22px;
@@ -59,6 +137,7 @@ class product extends Component {
 `}} /> 
 
             <Nav />
+
   <div className="site-blocks-cover inner-page-cover overlay" data-aos="fade" data-stellar-background-ratio="0.5">
       <div className="container">
         <div className="row align-items-center justify-content-center text-center">
@@ -85,17 +164,25 @@ class product extends Component {
       
       <div className="container">
         <div className="row">
-          <div className="col-md-7">
-            <img src="https://colorlib.com/preview/theme/chimper/images/img_1.jpg" alt="Image" className="img-fluid mb-4" />
-            <img src="https://colorlib.com/preview/theme/chimper/images/img_2.jpg" alt="Image" className="img-fluid mb-4" />
-            <img src="https://colorlib.com/preview/theme/chimper/images/img_3.jpg" alt="Image" className="img-fluid mb-4" />
-          </div>
-          <div className="col-md-4 ml-auto">
+
+<div className="col-md-4">
             <h3 className="mb-3">Case Study</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis itaque temporibus qui, est possimus porro in, molestiae consectetur dolores soluta, minima quas rem vel saepe. Dolor quibusdam perferendis officia magni.</p>
-            <p>Quae libero veritatis facilis consectetur, nostrum ab ipsa, asperiores dolorum sed omnis minus debitis, eaque quis provident nihil. Assumenda officia, dolorum commodi, accusamus beatae pariatur neque sapiente! Consequuntur, saepe, perspiciatis?</p>
-            <p>Nihil sit commodi natus perferendis, dolorem libero necessitatibus assumenda facilis aperiam atque. Illum accusantium, non saepe quo harum dicta, molestias amet maxime sequi, minus magni neque quos perspiciatis praesentium, atque!</p>
+             
           </div>
+          <div className="col-md-4">
+             <p id="lpsa">Name</p>
+                               <p id="lpsa">Name</p>
+             <p id="lpsa">Name</p>
+             <p id="lpsa">Name</p>
+
+          </div>
+
+          <div className="col-md-7">
+                                 <PhotoSwipeGallery items={items} options={options} thumbnailContent={getThumbnailContent}/>
+
+          </div>
+       
         </div>
       </div>
     </section>
