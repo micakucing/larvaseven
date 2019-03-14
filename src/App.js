f86c3adc314978ddd1fe5eef570070130ba48d57
mica neko <micakucing@gmail.com>
2019-03-14 11:23:40 +0700

    l

--- src/App.js
+++ src/App.js
@@ -6,14 +6,28 @@ import Headnav from "./route/home";
 import About from "./route/about";
 import Port from "./route/portfolio";
 import Kontak from "./route/contact";
-import DeviceOrientation from 'react-device-orientation';
 import $ from "jquery";
-
+import DeviceOrientation from 'react-device-orientation';
+import id from './translations/id.json';
+import en from './translations/en.json';
 import not from "./route/NotFound";
 
 
-
+var u = localStorage.getItem('myData');
+var y;
+if (u === null){
+    y = 'en';
+}else{
+    y = localStorage.getItem('myData');
+}
+ setDefaultTranslations({id, en})
+setDefaultLanguage(y)
 class App extends Component {
+constructor(props) {
+    super(props);
+    this.state = {addClassid: false, addClassen: false, land: false}
+}
+
 
   rotate = () =>{
     return(<DeviceOrientation>
@@ -28,7 +42,7 @@ class App extends Component {
         )
       }else if(Math.abs(gamma) === '90' && Math.abs(beta) === '90' && alpha === '0') {
           return (
-            <div d="kum" className="clox_tray"><img src="https://res.cloudinary.com/dsl4hppsk/image/upload/v1533604113/orientation.png" alt="" className="po"/><span className="rotator">{this.props.t('home.rotator')}</span>
+            <div id="kum" className="clox_tray"><img src="https://res.cloudinary.com/dsl4hppsk/image/upload/v1533604113/orientation.png" alt="" className="po"/><span className="rotator">{this.props.t('home.rotator')}</span>
 </div>
         )
       } else {
@@ -61,11 +75,34 @@ rots = () => {
 window.onorientationchange = this.readDeviceOrientation;
 }
   render() {
+
+let  b='idl' ,x='enl', com;
+const isLoggedIn = this.state.land;
+    if(this.state.addClassid) {
+      b = 'idl';
+      x = 'enl';
+}
+    if(this.state.addClassen) {
+      x = 'enl';
+      b = 'idl';
+}
+
+  if(isLoggedIn){
+    com =  <div id="kum" className="clox_tray">
+          <img src="https://res.cloudinary.com/dsl4hppsk/image/upload/v1533604113/orientation.png" className="po" alt=""/>
+          <span className="rotator">{this.props.t('home.rotator')}</span>
+          </div>
+  }else{
+    com = ''
+  }
+  
+
     return (
     	  <HashRouter>
 
       <div className="App">
         {this.rots()}
+         {com}
         <header className="App-header">  
 
  <Switch>
