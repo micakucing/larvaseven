63cd716f20d95897ef096b89c846dba9d18b3c1a
mica neko <micakucing@gmail.com>
2019-03-28 10:26:19 +0700

    l

--- src/route/NotFound.js
+++ src/route/NotFound.js
@@ -159,7 +159,7 @@ span {
   <Nav />
   <Helmet>
       <meta charSet="utf-8" />
-                <title>About us</title>
+                <title>Page not found</title>
              </Helmet>
   <div className="site-blocks-cover inner-page-cover overlay"  data-aos="fade" data-stellar-background-ratio="0.5">
       <div className="container">
